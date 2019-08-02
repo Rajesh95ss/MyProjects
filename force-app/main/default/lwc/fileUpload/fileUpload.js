8bd396fa1d8379a39eb080504dfd5a83b222f125
Rajesh95ss <prajeshkumar95s@gmail.com>
2019-08-02 15:25:28 +0530

    Initial

---
+++ force-app/main/default/lwc/fileUpload/fileUpload.js
@@ -0,0 +1,18 @@
+import {
+    LightningElement,
+    api
+} from 'lwc';
+export default class MyComponentName extends LightningElement {
+    @api recordId;
+
+    get acceptedFormats() {
+        return ['.pdf', '.png'];
+    }
+
+    handleUploadFinished(event) {
+        // Get the list of uploaded files
+        const uploadedFiles = event.detail.files;
+        /*eslint-disable*/
+        alert("No. of files uploaded : " + uploadedFiles.length);
+    }
+}
--- force-app/main/default/lwc/selectedContact/selectedContact.js
+++ force-app/main/default/lwc/selectedContact/selectedContact.js
@@ -1,6 +1,7 @@
 import {
     LightningElement,
-    api
+    api,
+    track
 } from 'lwc';
 import {
     NavigationMixin
@@ -8,17 +9,38 @@ import {
 
 export default class ViewTreeSelected extends NavigationMixin(LightningElement) {
     @api recordid;
+    @track
+    recordPageUrl;
+
     handleView() {
-        /* eslint-disable*/
-        alert();
-        // // Navigate to contact record page
-        // this[NavigationMixin.Navigate]({
-        //     type: 'standard__recordPage',
-        //     attributes: {
-        //         recordId: recordid,
-        //         objectApiName: 'Contact',
-        //         actionName: 'view',
-        //     },
-        // });
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.recordid,
+                objectApiName: 'Contact',
+                actionName: 'view'
+            },
+        });
+        //     /*eslint-disable*/
+        //     alert(this.recordId);
+        //     this[NavigationMixin.Navigate]({
+        //         type: 'standard__objectPage',
+        //         attributes: {
+        //             objectApiName: 'Contact',
+        //             actionName: 'home',
+        //         },
+        //     });
+        //     // Generate a URL to a User record page
+        //     /*eslint-disable*/
+        //     this[NavigationMixin.GenerateUrl]({
+        //         type: 'standard__recordPage',
+        //         attributes: {
+        //             recordId: '0037F00001Qp5aqQAB',
+        //             objectApiName: 'Contact',
+        //             actionName: 'view',
+        //         },
+        //     }).then(url => {
+        //         this.recordPageUrl = url;
+        //     });
     }
 }
